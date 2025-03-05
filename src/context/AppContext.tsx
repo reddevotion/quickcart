541683ac7e9097d6c1c58eae0539217a31f26822
Valery Reddevotion <linkolnsid228@gmail.com>
2025-03-05 08:23:51 +0300

    cartData

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -105,7 +105,7 @@ export const AppContextProvider = ({children} : {children : React.ReactNode}) =>
 
     const addToCart = async (itemId: string) => {
 
-        let cartData = structuredClone(cartItems);
+        let cartData = structuredClone(cartItems || {});
         if(!userData) {
             toast.error("You are not logged in");
             return;
